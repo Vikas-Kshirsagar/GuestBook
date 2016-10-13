eea9f516e0c4a8b6cf00eb43b8a8bf1e1c6494be
Vikas Kshirsagar <vikaskshirsagar@outlook.com>
2016-10-13 14:18:35 -0400

    Add files via upload

---
+++ signup_success.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Guestbook</title>
+  <link rel="stylesheet" href="css/base.css" type="text/css" media="screen" />
+  <link rel="stylesheet" id="current-theme" href="css/bec/style.css" type="text/css" media="screen" />  
+</head>
+<body>
+  <div id="container">
+    <div id="header">
+      <h1><a href="index.php">Guestbook</a></h1>
+      
+      
+    </div>
+    <div id="wrapper" class="wat-cf">
+      
+      
+    <div id="box">
+     
+      <div class="block" id="block-signup">
+        <h2>Sign up Success</h2>
+        <div class="content">
+            <div class="group wat-cf">
+              <div class="left">
+                <p class="first">Thanks for creating an account. To proceed please click on below link.</p>
+              </div>
+             
+            </div>
+            
+            <div class="group navform wat-cf">
+              <button class="button" type="submit" onclick="window.location='index.php';">
+                <img src="images/icons/tick.png" alt="Save" /> Login to continue.
+              </button>
+            </div>
+         </div>
+      </div>
+
+     
+    </div>
+  </div>
+
+</body>
+</html>
+
